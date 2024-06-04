be47e02a80178bc00b0b6d5097406e7f952641de
Yiming Zhang <104474082+zerojuhao@users.noreply.github.com>
2024-06-04 19:21:30 +0200

    Update trajectory_eva.py

--- isaacgymenvs/tasks/trajectory_eva.py
+++ isaacgymenvs/tasks/trajectory_eva.py
@@ -3,94 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import math
-def trajectory_eva_1(data):
-        
-    x_coords = data.iloc[:, 0]
-    y_coords = data.iloc[:, 1]
-    z_coords = data.iloc[:, 2]
-    num_elements = len(z_coords)
-
-    x_min, x_max = np.min(x_coords), np.max(x_coords)
-    y_min, y_max = np.min(y_coords), np.max(y_coords)
-    z_min, z_max = np.min(z_coords), np.max(z_coords)
-
-    cube_size = 0.1
-
-    x_edges = np.arange(x_min, x_max + cube_size, cube_size)
-    y_edges = np.arange(y_min, y_max + cube_size, cube_size)
-    z_edges = np.arange(z_min, z_max + cube_size, cube_size)
-
-    counts = np.zeros((len(x_edges), len(y_edges), len(z_edges)), dtype=int)
-
-    for x, y, z in zip(x_coords, y_coords, z_coords):
-        x_index = np.searchsorted(x_edges, x) - 1
-        y_index = np.searchsorted(y_edges, y) - 1
-        z_index = np.searchsorted(z_edges, z) - 1
-        counts[x_index, y_index, z_index] += 1
-
-    sum = 0
-    N = 0
-    sum_cube = 0
-
-    for i in range(counts.shape[0]):
-        for j in range(counts.shape[1]):
-            for k in range(counts.shape[2]):
-                N = N + counts[i,j,k]
-                sum = sum + counts[i,j,k]**2
-                # sum_cube = sum_cube + 1
-                # sum = sum + (counts[i,j,k]/(num_elements - cube_size**2))**2
-    # print("sum: ", sum)
-    # print("N: ", N)
-    C = np.log(sum / (N**2)) / np.log(cube_size)
-    # C = np.log(sum/(N))/np.log(cube_size)
-    # C = np.log(sum)/np.log(cube_size)
-    # C = sum
-    return C
-
-def trajectory_eva_2(data):
-
-    x_coords = data.iloc[:, 0]
-    y_coords = data.iloc[:, 1]
-    z_coords = data.iloc[:, 2]
-    num_elements = len(z_coords)
-
-    x_min, x_max = np.min(x_coords), np.max(x_coords)
-    y_min, y_max = np.min(y_coords), np.max(y_coords)
-    z_min, z_max = np.min(z_coords), np.max(z_coords)
-
-    cube_size = 0.1
-
-    x_edges = np.arange(x_min, x_max + cube_size, cube_size)
-    y_edges = np.arange(y_min, y_max + cube_size, cube_size)
-    z_edges = np.arange(z_min, z_max + cube_size, cube_size)
-
-    counts = np.zeros((len(x_edges), len(y_edges), len(z_edges)), dtype=int)
-
-    for x, y, z in zip(x_coords, y_coords, z_coords):
-        x_index = np.searchsorted(x_edges, x) - 1
-        y_index = np.searchsorted(y_edges, y) - 1
-        z_index = np.searchsorted(z_edges, z) - 1
-        counts[x_index, y_index, z_index] += 1
-
-    sum = 0
-    N = 0
-    sum_cube = 0
-
-    for i in range(counts.shape[0]):
-        for j in range(counts.shape[1]):
-            for k in range(counts.shape[2]):
-                N = N + counts[i,j,k]
-                # sum = sum + counts[i,j,k]**2
-                sum_cube = sum_cube + 1
-                sum = sum + (counts[i,j,k]/(num_elements - cube_size**2))**2
-    # print("sum: ", sum)
-    # print("N: ", N)
-    # C = np.log(sum / (N**2)) / np.log(cube_size)
-    C = np.log(sum/(N))/np.log(cube_size)
-    # C = np.log(sum)/np.log(cube_size)
-    # C = sum
-    return C
-
 
 def trajectory_eva_3(data):
     # print("data: ")
@@ -113,9 +25,7 @@ def trajectory_eva_3(data):
         angle_rad = np.arccos(angle_clip)
         angle_deg = np.degrees(angle_rad)
         angles.append(angle_deg)
-
-
-
+            
     # sun of lengths of the trajectory
     for i in range(num_elements-1):
         point1 = data.loc[i, ['X', 'Y', 'Z']]
@@ -123,25 +33,10 @@ def trajectory_eva_3(data):
         vector = point2 - point1
         norm_vector = np.linalg.norm(vector)
         lengths.append(norm_vector)
-        
-    # print("angles: ", sum(angles))
-    # print("lengths: ", sum(lengths))
+
     C = sum(angles)/np.log(sum(lengths))
-    # C = sum(curvature)/num_elements
-
-    
-    
-
-    return C
-
-
-
-
-
-
-
         
-
+    return C
 
 line_x = pd.read_csv('isaacgymenvs/tasks/trajectory/line_x.csv')
 line_xy = pd.read_csv('isaacgymenvs/tasks/trajectory/line_xy.csv')
@@ -164,49 +59,6 @@ tornado = pd.read_csv('isaacgymenvs/tasks/trajectory/tornado.csv')
 wheel = pd.read_csv('isaacgymenvs/tasks/trajectory/wheel.csv')
 roll = pd.read_csv('isaacgymenvs/tasks/trajectory/roll.csv')
 
-# m1
-# m1_line_x = trajectory_eva_1(line_x)
-# m1_line_xy = trajectory_eva_1(line_xy)
-# m1_line_xyz = trajectory_eva_1(line_xyz)
-# m1_circle = trajectory_eva_1(circle)
-# m1_ouroboros = trajectory_eva_1(ouroboros)
-# m1_ouroboros_z = trajectory_eva_1(ouroboros_z)
-# m1_ouroboros_plus = trajectory_eva_1(ouroboros_plus)
-# m1_ouroboros_plus_z = trajectory_eva_1(ouroboros_plus_z)
-# m1_corss = trajectory_eva_1(corss)
-# m1_ellipse = trajectory_eva_1(ellipse)
-
-# m1_playground = trajectory_eva_1(playground)
-# m1_retreat = trajectory_eva_1(retreat)
-# m1_sin = trajectory_eva_1(sin)
-# m1_spiral_v = trajectory_eva_1(spiral_v)
-# m1_spiral_h = trajectory_eva_1(spiral_h)
-# m1_swirl = trajectory_eva_1(swirl)
-# m1_tornado = trajectory_eva_1(tornado)
-# m1_wheel = trajectory_eva_1(wheel)
-# m1_roll = trajectory_eva_1(roll)
-
-# m2
-# m2_line_x = trajectory_eva_2(line_x)
-# m2_line_xy = trajectory_eva_2(line_xy)
-# m2_line_xyz = trajectory_eva_2(line_xyz)
-# m2_circle = trajectory_eva_2(circle)
-# m2_ouroboros = trajectory_eva_2(ouroboros)
-# m2_ouroboros_z = trajectory_eva_2(ouroboros_z)
-# m2_ouroboros_plus = trajectory_eva_2(ouroboros_plus)
-# m2_ouroboros_plus_z = trajectory_eva_2(ouroboros_plus_z)
-# m2_corss = trajectory_eva_2(corss)
-# m2_ellipse = trajectory_eva_2(ellipse)
-# m2_playground = trajectory_eva_2(playground)
-# m2_retreat = trajectory_eva_2(retreat)
-# m2_sin = trajectory_eva_2(sin)
-# m2_spiral_v = trajectory_eva_2(spiral_v)
-# m2_spiral_h = trajectory_eva_2(spiral_h)
-# m2_swirl = trajectory_eva_2(swirl)
-# m2_tornado = trajectory_eva_2(tornado)
-# m2_wheel = trajectory_eva_2(wheel)
-# m2_roll = trajectory_eva_2(roll)
-
 # m3
 m3_line_x = 0
 m3_line_xy = 0
@@ -227,51 +79,6 @@ m3_swirl = trajectory_eva_3(swirl)
 m3_tornado = trajectory_eva_3(tornado)
 m3_wheel = trajectory_eva_3(wheel)
 m3_roll = trajectory_eva_3(roll)
-
-
-# method 1
-# original_numbers = []
-# original_numbers.append(m1_line_x)
-# original_numbers.append(m1_line_xy)
-# original_numbers.append(m1_line_xyz)
-# original_numbers.append(m1_circle)
-# original_numbers.append(m1_ouroboros)
-# original_numbers.append(m1_ouroboros_z)
-# original_numbers.append(m1_ouroboros_plus)
-# original_numbers.append(m1_ouroboros_plus_z)
-# original_numbers.append(m1_corss)
-# original_numbers.append(m1_ellipse)
-# original_numbers.append(m1_playground)
-# original_numbers.append(m1_retreat)
-# original_numbers.append(m1_sin)
-# original_numbers.append(m1_spiral_v)
-# original_numbers.append(m1_spiral_h)
-# original_numbers.append(m1_swirl)
-# original_numbers.append(m1_tornado)
-# original_numbers.append(m1_wheel)
-# original_numbers.append(m1_roll)
-
-# method 2
-# original_numbers = []
-# original_numbers.append(m2_line_x)
-# original_numbers.append(m2_line_xy)
-# original_numbers.append(m2_line_xyz)
-# original_numbers.append(m2_circle)
-# original_numbers.append(m2_ouroboros)
-# original_numbers.append(m2_ouroboros_z)
-# original_numbers.append(m2_ouroboros_plus)
-# original_numbers.append(m2_ouroboros_plus_z)
-# original_numbers.append(m2_corss)
-# original_numbers.append(m2_ellipse)
-# original_numbers.append(m2_playground)
-# original_numbers.append(m2_retreat)
-# original_numbers.append(m2_sin)
-# original_numbers.append(m2_spiral_v)
-# original_numbers.append(m2_spiral_h)
-# original_numbers.append(m2_swirl)
-# original_numbers.append(m2_tornado)
-# original_numbers.append(m2_wheel)
-# original_numbers.append(m2_roll)
 
 # method 3
 original_numbers = []
